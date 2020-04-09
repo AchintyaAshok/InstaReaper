089cbde4486fa0b435c567f6494c71a3e9b543ca
Ashok Krishnamurthi <ashokkrishnamurthi@Ashoks-MacBook-Pro.local>
2020-04-09 16:28:09 -0700

    Added a way to send Texts instead of emails, using a shell script ( file is send_text.sh)

---
+++ src/send_text.sh
@@ -0,0 +1,3 @@
+osascript -e 'tell application "Messages" to send "Instacart Slot Available Now!" to buddy "Deepa Iyengar"'
+osascript -e 'tell application "Messages" to send "Instacart Slot Available Now!" to buddy "Instacart"'
+
